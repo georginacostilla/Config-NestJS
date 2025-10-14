fec8b67a43ff869964560f2c85893933b15ddc4e
georginacostilla <georginacostilla.gc@gmail.com>
2025-10-14 15:59:41 -0300

    se agregan las primeras configuraciones iniciales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') ?? 3000;
+  const NODE_ENV = configService.get<string>('NODE_ENV');
+
+  await app.listen(PORT, () => {
+    Logger.log(
+      `Application running the port: http://localhost:${PORT}`,
+      NestApplication.name
+    );
+    Logger.log(`Current Environment: ${NODE_ENV}`, NestApplication.name);
+  });
 }
 bootstrap();
