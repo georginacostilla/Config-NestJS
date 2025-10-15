9ca61c189729cd23db6f98618c86a252e93377c1
georginacostilla <georginacostilla.gc@gmail.com>
2025-10-15 01:48:53 -0300

    se agregaron nuevas configuraciones

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestApplication, NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ClassSerializerInterceptor } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { corsOptions } from './config/cors.config';
+import { setupSwagger } from './config/swagger-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.enableCors(corsOptions);
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      excludePrefixes: ['password', 'createdAt', 'updatedAt', 'isDeleted'],
+      ignoreDecorators: true,
+    }),
+  );
+
+  setupSwagger(app);
 
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT') ?? 3000;
